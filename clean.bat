5f46f3b9fdf9c056d4a86a59e386305d5c2a1c4b
Angela945 <Angela945@users.noreply.github.com>
2018-09-23 14:33:29 +0800

    Latex template for Sun Yat-sen Univeristy Matser Thesis
    Latex template for Sun Yat-sen Univeristy Matser Thesis

---
+++ clean.bat
@@ -0,0 +1,23 @@
+del *.aux /s
+del *.bak /s
+del *.log /s
+del *.bbl /s
+del *.dvi /s
+del *.blg /s
+del *.thm /s
+del *.toc /s
+del *.toe /s
+del *.lof
+del *.lot
+del *.out
+del *.ps
+del *.snm
+del *.nav
+del *.spl
+del *.synctex.gz
+del *.tex.sav
+del *.pdfsync
+del *.tex.bak
+del *.synctex
+del *.bib.sav
+del *.synctex.gz(busy)
